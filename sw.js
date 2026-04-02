cf880b018e1276cd9b3169d13591325f7881a0ee
zaidmanm <zaidmanm@gmail.com>
2026-04-02 13:06:20 -0400

    Bump service worker version to retrocade-4

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 //
 // ═══════════════════════════════════════════════════════════════════
 
-const VERSION = 'retrocade-3';   // ← BUMP THIS NUMBER TO PUSH AN UPDATE
+const VERSION = 'retrocade-4';   // ← BUMP THIS NUMBER TO PUSH AN UPDATE
 
 // Files to pre-cache on install (relative to the sw.js location)
 const PRECACHE = [
